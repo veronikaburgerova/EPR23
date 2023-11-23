cf3c3f6d5ac63cdd20145041b5e2fb5e550280d9
Jan Horacek <me@apophis.cz>
2023-11-23 07:12:40 +0100

    Add code with ProgressBar

---
+++ WPFPrvky/WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            Button button = (Button)sender;
+            if (button.IsLoaded)
+                progressbar1.Value += int.Parse(button.Content.ToString());
+
+            if (progressbar1.Value >= progressbar1.Maximum)
+                MessageBox.Show("Hotovo");
+        }
+    }
+}
